52de2dd707656a8464d78ba615da41c725249fe0
MinSeong_Jung <jms9896@gmail.com>
2024-10-15 15:53:37 +0900

    .

--- MarkDirtyScript.cs
+++ MarkDirtyScript.cs
@@ -5,15 +5,6 @@ public class MarkDirtyScript : MonoBehaviour
 {
 
     public RenderTexture renderTexture;
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         PolySpatialObjectUtils.MarkDirty(renderTexture);
